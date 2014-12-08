fb827e0eac9047b16b08c70df40166a36f6dd4cc
Garhan Attebury <garhan.attebury@unl.edu>
2014-12-08 11:53:34 -0600

    real initial commit

---
+++ manifests/client.pp
@@ -0,0 +1,33 @@
+#
+# openldap::client
+#
+
+class openldap::client (
+    $uri            = $openldap::params::uri,
+    $base           = $openldap::params::base,
+    $tls_cacert     = $openldap::params::tls_cacert,
+    $tls_cacertdir  = $openldap::params::tls_cacertdir,
+    $tls_reqcert    = $openldap::params::tls_reqcert,
+) inherits openldap::params {
+
+    validate_string($uri)
+    validate_string($base)
+    validate_absolute_path($tls_cacert)
+    validate_absolute_path($tls_cacertdir)
+    validate_bool($tls_reqcert)
+
+    package { 'openldap':
+        ensure => 'present'
+    }
+
+    file { '/etc/openldap/ldap.conf':
+        ensure  => 'present',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        require => Package['openldap'],
+        content => template('openldap/openldap-ldap.conf.erb'),
+    }
+
+}
+
---
+++ manifests/params.pp
@@ -0,0 +1,14 @@
+#
+# openldap::params
+#
+
+class openldap::params {
+
+    $uri            = undef
+    $base           = undef
+    $tls_cacert     = undef
+    $tls_cacertdir  = undef
+    $tls_reqcert    = false
+
+}
+
